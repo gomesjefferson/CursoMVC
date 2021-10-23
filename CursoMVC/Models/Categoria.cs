e855037deb387a3efa5f09410a2dc97888af3568
Jefferson Gomes <jefferson_gomes10@hotmail.com>
2021-10-23 12:36:11 -0300

    Criado um projeto de API e feito pequenas alterações no Projeto CursoMVC

--- CursoMVC/Models/Categoria.cs
+++ CursoMVC/Models/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace CursoMVC.Models
     {
         public List<Produtos> Produtos { get; set; }
         public int Id {get; set; }
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
     }
 }
--- CursoMVC/Models/Produtos.cs
+++ CursoMVC/Models/Produtos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace CursoMVC.Models
     public class Produtos
     {
         public int Id { get; set; }
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
         public int CategoriaId { get; set; }
         public Categoria Categoria { get; set; }
